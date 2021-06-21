957c074efa99dc47097c1f644d1be9dcde2f8b43
Nam Trần <nhatnam99132@gmail.com>
2021-06-21 10:22:16 +0700

    Add project files.

---
+++ InventoryManagement/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using InventoryManagement.Data;
+using InventoryManagement.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(InventoryManagement.Areas.Identity.IdentityHostingStartup))]
+namespace InventoryManagement.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<InventoryManagementContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("InventoryManagementContextConnection")));
+
+                services.AddDefaultIdentity<Employee>(options => options.SignIn.RequireConfirmedAccount = false)
+                    .AddEntityFrameworkStores<InventoryManagementContext>();
+            });
+        }
+    }
+}
---
+++ InventoryManagement/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Models
+{
+    public class Employee : IdentityUser
+    {
+        public string EmployeeName { get; set; }
+        public string EmployeeAddress { get; set; }
+        public bool? Gender { get; set; }
+        public DateTime? Birthdate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
+
